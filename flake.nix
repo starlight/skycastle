9c7cc7a71f068315f5694f75a2796fe9f6f96f01
arizona <arizona@starlig.ht>
2023-06-04 10:14:45 -0400

    refactor template

--- flake.nix
+++ flake.nix
@@ -2,21 +2,28 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.install = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.install-iso = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
         ./modules/install.nix
       ];
     };
+
     nixosModules = {
       desktop = ./modules/desktop.nix;
       minimal = ./modules/minimal.nix;
     };
-    templates.default = {
-      path = ./templates/system;
-      description = "skycastle system template";
-      welcomeText = "skycastle system template applied";
+
+    templates = let
+      nixosTemplate = {
+        path = ./templates/nixos;
+        description = "nixos system template";
+        welcomeText = "nixos system applied!";
+      };
+    in {
+      default = nixosTemplate;
+      nixos = nixosTemplate;
     };
   };
 }
