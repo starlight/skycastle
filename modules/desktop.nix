df1faa5f495c000180197028037ce0e1ebe0e8a3
arizona <arizona@starlig.ht>
2023-06-03 21:43:56 -0400

    +bspwm

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -5,6 +5,12 @@
   #options = {};
 
   config = {
+    services.xserver = {
+      enable = true;
+      windowManager.bspwm.enable = true;
+      displayManager.startx.enable = true;
+      desktopManager.xterm.enable = true;
+    };
     environment.systemPackages = with pkgs; [
       firefox
     ];
