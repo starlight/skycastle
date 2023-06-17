9fb735c513a62ba81f770b7569b03feca76339a3
arizona <arizona@starlig.ht>
2023-06-17 18:16:39 -0400

    flake update

--- modules/install.nix
+++ modules/install.nix
@@ -1,26 +1,26 @@
 { config, pkgs, ... }:
 
 {
-  # imports = [];
-  # options = {};
-
   config = {
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
-    programs.vim.defaultEditor = true;
+
     environment.systemPackages =
-    let
-      skycastle-generate-config = pkgs.writeShellScriptBin "skycastle-generate-config" ''
-        nixos-generate-config --root /mnt || exit 1
-        cd /mnt/etc/nixos || exit 1
-        exec nix flake init -t github:starlight/skycastle#nixos
-      '';
-      skycastle-install = pkgs.writeShellScriptBin "skycastle-install" ''
-        exec nixos-install --flake /mnt/etc/nixos#skycastle
-      '';
-    in [
-      skycastle-generate-config
-      skycastle-install
-    ];
+      let
+        skycastle-generate-config = pkgs.writeShellScriptBin "skycastle-generate-config" ''
+          mkdir -p /mnt/etc/nixos || exit 1
+          cd /mnt/etc/nixos || exit 1
+          nix flake init -t github:starlight/skycastle#nixos || exit 1
+          exec nixos-generate-config --root /mnt $@
+        '';
+        skycastle-install = pkgs.writeShellScriptBin "skycastle-install" ''
+          exec nixos-install --flake /mnt/etc/nixos#skycastle $@
+        '';
+      in [
+        skycastle-generate-config
+        skycastle-install
+      ];
+
+    programs.vim.defaultEditor = true;
   };
 }
 
