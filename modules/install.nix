4a244367263cafc5ed02ea928c65decc9f6b60e1
arizona <arizona@starlig.ht>
2023-06-17 16:04:10 -0400

    install wrappers for flake

--- modules/install.nix
+++ modules/install.nix
@@ -7,6 +7,20 @@
   config = {
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
     programs.vim.defaultEditor = true;
+    environment.systemPackages =
+    let
+      skycastle-generate-config = pkgs.writeShellScriptBin "skycastle-generate-config" ''
+        nixos-generate-config --root /mnt || exit 1
+        cd /mnt/etc/nixos || exit 1
+        exec nix flake init -t github:starlight/skycastle#nixos
+      '';
+      skycastle-install = pkgs.writeShellScriptBin "skycastle-install" ''
+        exec nixos-install --flake /mnt/etc/nixos#skycastle
+      '';
+    in [
+      skycastle-generate-config
+      skycastle-install
+    ];
   };
 }
 
